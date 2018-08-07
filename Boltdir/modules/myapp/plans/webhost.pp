8463de3d6b692a1e55689204f583295a02d94e83
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2018-08-07 15:56:05 +0200

    Create webhost.pp

---
+++ Boltdir/modules/myapp/plans/webhost.pp
@@ -0,0 +1,23 @@
+plan myapp::webhost(
+) {
+  # Prep this node for applying Puppet code (doesn't work yet)
+  apply_prep('localhost')
+  #run_task('puppet_agent::install', 'localhost')
+
+  # Retrieve facts
+  #run_plan('facts', nodes => 'localhost')
+
+  # Apply SampleApp prereqs
+  $report = apply('localhost') {
+
+    nginx::resource::server { 'www.myapp.com':
+      listen_port => 80,
+      www_root    => '/var/www/myapp',
+      index_files => ['index.html'],
+    }
+
+  }
+
+  # return $report
+
+}
